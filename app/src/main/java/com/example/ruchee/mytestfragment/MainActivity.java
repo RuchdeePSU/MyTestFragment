cccaf6cb35a2cb0219af485111bd128c554c2ff1
Ruchdee <ruchy.tts@gmail.com>
2016-11-19 09:21:48 +0700

    first commit

---
+++ app/src/main/java/com/example/ruchee/mytestfragment/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.ruchee.mytestfragment;
+
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        this.setSupportActionBar(mToolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.action_fragment1:
+                FragmentOne f1 = new FragmentOne();
+                FragmentTransaction f1Transaction = getSupportFragmentManager().beginTransaction();
+                f1Transaction.replace(R.id.fragment_container, f1);
+                f1Transaction.commit();
+            case R.id.action_fragment2:
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
